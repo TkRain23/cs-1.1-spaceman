222579d6312c697ebc98068a4698987dd2dc59d8
tkrain23 <tk.rain09@gmail.com>
2018-09-12 15:34:17 -0700

    new attempt

--- spaceman.py
+++ spaceman.py
@@ -1,60 +1,41 @@
 import random  # import library of functions -- random numbers
-import collections
 
 
 def load_word():
+    """
+    load_word: Reads a text file and stores each word into a list.
+    The list of words are separated by a single space.
+    It then returns a random word from the list that will be used for the game.
+    """
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close
 
     words_list = words_list[0].split(' ')
     secret_word = random.choice(words_list)
-    print(list(secret_word))
+    # print(list(secret_word))
     return secret_word
 
 
-def is_word_guessed(secret_word, letters_guessed):
-    '''
-    secretWord: string, the random word the user is trying to guess.
-    This is selected on line 9.
-    lettersGuessed: list of letters that have been guessed so far.
-    returns: boolean,
-    True only if all the letters of secretWord are in lettersGuessed;
-    False otherwise
-    '''
-    # checks to see if the length matches first
-    if length.secret_word == length.letters_guessed:
-        # turns secret_word into a list of characters
-        # compares it to the list of letters guessed
-        if set(list(secret_word)) == set(letters_guessed):
-            return true
-    else:
-        return false
+def display_board(secret_word):
+    for i in range(len(list(secret_word))-1):
+        print("_", end=' ')
+
+    print("_")
 
 
-def get_guessed_word(secret_word, letters_guessed):
-    '''
-    secretWord: string, the random word the user is trying to guess.
-    This is selected on line 9.
-    lettersGuessed: list of letters that have been guessed so far.
-    returns: string, of letters and underscores.
-    For letters in the word that the user has guessed correctly,
-    the string should contain the letter at the correct position.
-    For letters in the word that the user has not yet guessed,
-    shown an _ (underscore) instead.
-    '''
+
+def start_game(secret_word):
+    print("Your secret word contains {} characters.".format(len(secret_word)))
+    print("Guess one letter per round -- good luck!")
+    display_board(secret_word)
 
 
-def get_available_letters(letters_guessed):
-    '''
-    lettersGuessed: list of letters that have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
-      yet been guessed.
-    '''
-    theAlphabet = []
-    for letter in range(97, 123):
-        theAlphabet.append(chr(letter))
-    myAlphabet = theAlphabet[:]
+def spaceman(function_code):
+    print("Welcome to Spaceman: Press 'A' to Begin")
+    if function_code == "A" or function_code == "a":
+        # load the game
+        start_game(load_word())
 
 
-load_word()
+spaceman("A")
